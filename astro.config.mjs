a2accbe811e4d858ed2fe4b53406a281fb7c0cdb
Stefan <67121265+Naainz@users.noreply.github.com>
2024-08-18 15:42:12 +0300

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   integrations: [tailwind(), mdx()],
+  adapter: vercel(),
   vite: {
     resolve: {
       alias: {
