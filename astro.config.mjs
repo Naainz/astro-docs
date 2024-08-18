81a5434d23d19a2059c938617c9690c454ad0a60
Stefan <67121265+Naainz@users.noreply.github.com>
2024-08-18 11:53:42 +0300

    try

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
+  vite: {
+    resolve: {
+      alias: {
+        '@components': '/src/components', 
+        '@layouts': '/src/layouts',       
+      },
+    },
+  },
 });
